2ca4cecb0216438fab98ccf0cfd20fc7abcdeaca
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-08 00:11:02 +0300

    market page meal kits fetch

--- frontend/src/components/MarketFeaturedMeals.jsx
+++ frontend/src/components/MarketFeaturedMeals.jsx
@@ -1,14 +1,26 @@
-import FeaturedMeals from '../data/FeaturedMeals';
+// import FeaturedMeals from '../data/FeaturedMeals';
 import MarketMeals from '../components/MarketMeals';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const MarketFeaturedMeals = () => {
+  const [mealKits, setMealKits] = useState([]);
+  useEffect(() => {
+    const fetchMealKits = async () => {
+      const { data } = await axios('/api/mealKits');
+      setMealKits(data);
+    };
+
+    fetchMealKits();
+  }, []);
+
   return (
     <section>
       <div className="w-[62%] m-auto">
         <h3 className="text-[22px] mb-[24px] fontCera font-semibold text-[#303236] w-fit m-auto">Featured</h3>
         <div className="flex flex-row justify-center items-center flex-wrap gap-4 ">
-          {FeaturedMeals.map((meal, index) => {
-            return <MarketMeals meal={meal} key={index} />;
+          {mealKits.map((mealKit, index) => {
+            return <MarketMeals mealKit={mealKit} key={index} />;
           })}
         </div>
       </div>
--- frontend/src/components/MarketMeals.jsx
+++ frontend/src/components/MarketMeals.jsx
@@ -1,12 +1,24 @@
-const MarketMeals = ({ meal }) => {
+/* eslint-disable react/prop-types */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const MarketMeals = ({ mealKit }) => {
+  const [mealImg, setMealImg] = useState('');
+  useEffect(() => {
+    const fetchSingleMail = async () => {
+      const { data } = await axios.get(`/api/meals/${mealKit.meals[0].meal}`);
+      setMealImg(data.img);
+    };
+
+    fetchSingleMail();
+  }, [mealKit.meals]);
   return (
     <div className="fontCera w-[282px] mb-[20px] h-[394px]">
-      <img src={meal.src} />
+      <img src={mealImg} />
       <a href="" className="font-semibold text-[#303236] inline-block mt-4 hover:underline">
-        {meal.title}
+        {mealKit.name}
       </a>
-      <p className="text-[#6a6d75] text-[15px] leading-[18px]">{meal.description}</p>
-      <span className="text-[#303236] inline-block mt-[12px]">{meal.price}</span>
+      <p className="text-[#6a6d75] text-[15px] leading-[18px]">{mealKit.subTxt}</p>
+      <span className="text-[#303236] inline-block mt-[12px]">${mealKit.price}</span>
     </div>
   );
 };
