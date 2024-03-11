d1a6e4bd0d61da9574934e65093728045ffce121
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-11 03:14:16 +0300

    sabit api değişkenleri

---
+++ frontend/src/constants.jsx
@@ -0,0 +1,7 @@
+// export const BASE_URL = process.env.NODE_ENV === 'development' ? 'https://localhost:5000' : '';
+export const BASE_URL = '';
+
+export const MEALS_URL = '/api/meals';
+export const MEALKITS_URL = '/api/mealKits';
+export const USERS_URL = '/api/users';
+export const ORDERS_URL = '/api/orders';
