6537c5761392d31aa9b1322b6020678b5a26177f
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-13 01:54:48 +0300

    axios yerine react-redux ile endpointlere ulaşma işlemi

--- frontend/src/components/MarketFeaturedMeals.jsx
+++ frontend/src/components/MarketFeaturedMeals.jsx
@@ -1,30 +1,27 @@
-// import FeaturedMeals from '../data/FeaturedMeals';
 import MarketMeals from '../components/MarketMeals';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useGetMealKitsQuery } from '../slices/mealKitsApiSlice';
+import Loader from '../pages/Loader';
 
 const MarketFeaturedMeals = () => {
-  const [mealKits, setMealKits] = useState([]);
-  useEffect(() => {
-    const fetchMealKits = async () => {
-      const { data } = await axios('/api/mealKits');
-      setMealKits(data);
-    };
-
-    fetchMealKits();
-  }, []);
+  const { data: mealKits, isLoading } = useGetMealKitsQuery();
 
   return (
-    <section>
-      <div className="w-[62%] m-auto">
-        <h3 className="text-[22px] mb-[24px] fontCera font-semibold text-[#303236] w-fit m-auto">Featured</h3>
-        <div className="flex flex-row justify-center items-center flex-wrap gap-4 ">
-          {mealKits.map((mealKit, index) => {
-            return <MarketMeals mealKit={mealKit} key={index} />;
-          })}
-        </div>
-      </div>
-    </section>
+    <>
+      {isLoading ? (
+        <Loader></Loader>
+      ) : (
+        <section>
+          <div className="w-[62%] m-auto">
+            <h3 className="text-[22px] mb-[24px] fontCera font-semibold text-[#303236] w-fit m-auto">Featured</h3>
+            <div className="flex flex-row justify-center items-center flex-wrap gap-4 ">
+              {mealKits.map((mealKit, index) => {
+                return <MarketMeals mealKit={mealKit} key={index} />;
+              })}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
--- frontend/src/components/MarketMeals.jsx
+++ frontend/src/components/MarketMeals.jsx
@@ -1,28 +1,26 @@
 /* eslint-disable react/prop-types */
-import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useGetMealDetailsQuery } from '../slices/mealsApiSlice';
+import Loader from '../pages/Loader';
 const MarketMeals = ({ mealKit }) => {
-  const [mealImg, setMealImg] = useState('');
-  useEffect(() => {
-    const fetchSingleMail = async () => {
-      const { data } = await axios.get(`/api/meals/${mealKit.meals[0].meal}`);
-      setMealImg(data.img);
-    };
-
-    fetchSingleMail();
-  }, [mealKit.meals]);
+  const { data: firstMealOfKit, isLoading } = useGetMealDetailsQuery(mealKit.meals[0].meal);
   return (
-    <div className="fontCera w-[282px] mb-[20px] h-[394px]">
-      <Link to={`/market/mealKit/${mealKit._id}`}>
-        <img src={mealImg} className="cursor-pointer" />
-      </Link>
-      <Link to={`/market/mealKit/${mealKit._id}`} className="font-semibold text-[#303236] inline-block mt-4 hover:underline cursor-pointer">
-        {mealKit.name}
-      </Link>
-      <p className="text-[#6a6d75] text-[15px] leading-[18px]">{mealKit.subTxt}</p>
-      <span className="text-[#303236] inline-block mt-[12px]">${mealKit.price}</span>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader></Loader>
+      ) : (
+        <div className="fontCera w-[282px] mb-[20px] h-[394px]">
+          <Link to={`/market/mealKit/${mealKit._id}`}>
+            <img src={firstMealOfKit.img} className="cursor-pointer" />
+          </Link>
+          <Link to={`/market/mealKit/${mealKit._id}`} className="font-semibold text-[#303236] inline-block mt-4 hover:underline cursor-pointer">
+            {mealKit.name}
+          </Link>
+          <p className="text-[#6a6d75] text-[15px] leading-[18px]">{mealKit.subTxt}</p>
+          <span className="text-[#303236] inline-block mt-[12px]">${mealKit.price}</span>
+        </div>
+      )}
+    </>
   );
 };
 
