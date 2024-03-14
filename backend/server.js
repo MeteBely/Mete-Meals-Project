068217af9a51599afa2c61e53c5f013fb2a2fc8f
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-14 20:06:29 +0300

    user routes eklenmesi ve req.body, req.cookie parserlar

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import connectMongoDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import mealRoutes from './routes/mealRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import mealKitsRoutes from './routes/mealKitsRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
 connectMongoDB();
+
+//Body Parser
+app.use(express.json()); //req.body --> raw
+app.use(express.urlencoded({ extended: true })); //req.body --> urlencoded
+app.use(cookieParser()); //req.cookie
 
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -17,6 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/meals', mealRoutes);
 app.use('/api/mealKits', mealKitsRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
