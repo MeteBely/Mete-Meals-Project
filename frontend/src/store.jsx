5715861b3ddad3844e9ccb6c4d59ba3cfdfbbe65
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-14 20:04:56 +0300

    auth slice

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice.jsx';
 import cartSliceReducer from './slices/cartSlice.jsx';
+import authSliceReducer from './slices/authSlice.jsx';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer, //webde redux state tarafında görüntüleyebilirsin. cart ismiyle tutulur.
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
