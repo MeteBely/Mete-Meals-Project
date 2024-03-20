369333c820736320690e7a328000d1a0a92ec31c
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-21 00:13:00 +0300

    localdaki userınfoya göre admin route kontrolü

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+const AdminRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  return <>{userInfo && userInfo.isAdmin ? <Outlet /> : <div className="mt-20">You are not authorized for an admin!</div>}</>;
+};
+
+export default AdminRoute;
