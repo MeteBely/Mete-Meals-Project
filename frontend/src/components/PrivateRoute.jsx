5556b1d334bfad23dfa41de22d74ad2a3f7ba232
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-21 00:10:12 +0300

    route fix

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 const PrivateRoute = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  return <>{userInfo ? <Outlet /> : <Navigate to={'users/sing_in'} replace />}</>;
+  return <>{userInfo ? <Outlet /> : <Navigate to={'users/sign_in'} replace />}</>;
 };
 
 export default PrivateRoute;
