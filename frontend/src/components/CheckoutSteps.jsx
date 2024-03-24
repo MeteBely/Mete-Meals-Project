fd5bf2acaa9cee6c49fc048c9cd9ccff968e6959
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-25 01:09:01 +0300

    checkout steps underline
    kullanıcı bulunduğu sayfaya göre componenntde underline belirir

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,41 +1,50 @@
+/* eslint-disable no-unused-vars */
 import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
-const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+const CheckoutSteps = ({ step1, step2, step3, step4, underline }) => {
   return (
-    <nav className="flex flex-row gap-4">
+    <nav className="flex flex-row gap-6">
       <div>
         {step1 ? (
-          <Link to="/users/sign_in">Sign In</Link>
+          <Link to="/users/sign_in" className={`text-[18px] ${underline === 'sign in' ? 'underline underline-offset-4' : 'no-underline'} text-[#0F346C]`}>
+            Sign In
+          </Link>
         ) : (
-          <Link aria-disabled className="text-[#A0A6A6]">
+          <Link aria-disabled className="text-[#A0A6A6] text-[18px]">
             Sign In
           </Link>
         )}
       </div>
       <div>
         {step2 ? (
-          <Link to="/shipping">Shipping</Link>
+          <Link to="/shipping" className={`text-[18px] ${underline === 'shipping' ? 'underline underline-offset-4' : 'no-underline'} text-[#0F346C]`}>
+            Shipping
+          </Link>
         ) : (
-          <Link aria-disabled className="text-[#A0A6A6]">
+          <Link aria-disabled className="text-[#A0A6A6] text-[18px]">
             Shipping
           </Link>
         )}
       </div>
       <div>
         {step3 ? (
-          <Link to="/payment">Payment</Link>
+          <Link to="/payment" className={`text-[18px] ${underline === 'payment' ? 'underline underline-offset-4' : 'no-underline'} text-[#0F346C]`}>
+            Payment
+          </Link>
         ) : (
-          <Link aria-disabled className="text-[#A0A6A6]">
+          <Link aria-disabled className="text-[#A0A6A6] text-[18px]">
             Payment
           </Link>
         )}
       </div>
       <div>
         {step4 ? (
-          <Link to="/placeorder">Place Order</Link>
+          <Link to="/placeorder" className={`text-[18px] ${underline === 'place order' ? 'underline underline-offset-4' : 'no-underline'} text-[#0F346C]`}>
+            Place Order
+          </Link>
         ) : (
-          <Link aria-disabled className="text-[#A0A6A6]">
+          <Link aria-disabled className="text-[#A0A6A6] text-[18px]">
             Place Order
           </Link>
         )}
