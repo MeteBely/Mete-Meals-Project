2f5dee2eab80696e68eba666db8ec26498c8943a
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-25 19:18:03 +0300

    order items düzeltmeleri

--- frontend/src/components/OrderItem.jsx
+++ frontend/src/components/OrderItem.jsx
@@ -10,11 +10,15 @@ const OrderItem = ({ orderItem }) => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="flex flex-row">
-          <img className="w-40" src={firstMealOfKit.img} alt={orderItem.name} />
-          <Link to={`/product/${orderItem.product}`}>{orderItem.name}</Link>
-          <div>
-            {orderItem.qty} x {orderItem.price} = ${orderItem.qty * orderItem.price}
+        <div className="flex flex-row items-center justify-between w-[500px] mb-2">
+          <div className="flex flex-row items-center justify-start">
+            <img className="w-20 rounded-md mr-4" src={firstMealOfKit.img} alt={orderItem.name} />
+            <Link className="fontCera underline" to={`/market/mealKit/${orderItem._id}`}>
+              {orderItem.name}
+            </Link>
+          </div>
+          <div className="fontCera">
+            {orderItem.qty} x ${orderItem.price} = ${orderItem.qty * orderItem.price}
           </div>
         </div>
       )}
--- frontend/src/components/PlaceOrderItems.jsx
+++ frontend/src/components/PlaceOrderItems.jsx
@@ -11,11 +11,15 @@ const PlaceOrderItems = ({ cartItem }) => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div className="flex flex-row">
-          <img className="w-40" src={firstMealOfKit.img} alt={cartItem.name} />
-          <Link to={`/meal/${cartItem._id}`}>{cartItem.name}</Link>
-          <div>
-            {cartItem.qty} x {cartItem.price} = {cartItem.qty * cartItem.price}
+        <div className="flex flex-row items-center justify-between w-[500px] mb-2">
+          <div className="flex flex-row items-center justify-start">
+            <img className="w-20 rounded-md mr-4" src={firstMealOfKit.img} alt={cartItem.name} />
+            <Link className="fontCera underline" to={`/market/mealKit/${cartItem._id}`}>
+              {cartItem.name}
+            </Link>
+          </div>
+          <div className="fontCera">
+            {cartItem.qty} x ${cartItem.price} = ${cartItem.qty * cartItem.price}
           </div>
         </div>
       )}
