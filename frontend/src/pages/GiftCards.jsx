8264d67dbfb1c8645a24a668ce8b472c4a4b4875
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-26 21:19:47 +0300

    Update GiftCards.jsx

--- frontend/src/pages/GiftCards.jsx
+++ frontend/src/pages/GiftCards.jsx
@@ -32,6 +32,7 @@ const GiftCards = () => {
     <main>
       <section className="mt-[60px] border-t-[1px] border-[#ECEEF2] bg-[#fafbfc]">
         <GiftCardsTopPanel />
+        deneme
         <div className="flex flex-col min-[1250px]:flex-row justify-center min-[1250px]:justify-start items-center min-[1250px]:items-start m-auto w-[62%] mt-12 gap-2 min-[1250px]:gap-12">
           <GiftCardsFirstCol amount={amount} />
           <GiftCardsSecondCol setAmount={setAmount} quantity={quantity} setQuantity={setQuantity} cart={cart} amount={amount} setCart={setCart} />
