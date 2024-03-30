80c70c90aa762a92072b949c4945a27e431bbd43
MeteBely <metehanmuradoglu17@gmail.com>
2024-03-31 00:21:59 +0300

    kod kullanma işlemi
    Db'de oluşturulan gift card bu sayfada çekilir ve silinir, çekilen gift cardın amountu dbdeki user balanceye eklenir. Eğer balancesi yoksa amount'a eşit şekilde balance olacak şekilde oluşturulur.

---
+++ frontend/src/pages/RedeemGiftCardBalance.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+// import CustomInput from '../components/FormComponents/CustomInput';
+import { useGetGiftCardByIdAndDeleteMutation } from '../slices/giftCardApiSlice';
+import { useUpdateToUserBalanceMutation } from '../slices/balanceApiSlice';
+
+const RedeemGiftCardBalance = () => {
+  const [giftCardId, setGiftCardId] = useState('');
+  const [getGiftCardAndDelete, { isLoading }] = useGetGiftCardByIdAndDeleteMutation();
+  const [updateUserBalance, { isLoading: userBalance }] = useUpdateToUserBalanceMutation();
+  const submithandler = async (e) => {
+    e.preventDefault();
+    const res = await getGiftCardAndDelete(giftCardId);
+    console.log(res.data.amount);
+    let amount = res.data.amount;
+    await updateUserBalance({ amount });
+  };
+
+  return (
+    <section>
+      <div className="w-[800px] m-auto mt-20">
+        <form action="" onSubmit={(e) => submithandler(e)}>
+          {/* <div>alo</div> */}
+          {/* <CustomInput onChange={(e) => setGiftCardKey(e.target.value)} value={giftCardKey} label="GIFTCARDKEY" name="giftCardCode" type="text" placeholder="Sign your gift card key here..." /> */}
+          <input type="text" onChange={(e) => setGiftCardId(e.target.value)} value={giftCardId} placeholder="Sign your gift card key here..." />
+          <button type="submit">BAS KANKA</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default RedeemGiftCardBalance;
