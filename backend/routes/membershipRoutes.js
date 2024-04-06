88117dbca2b9bcab83580b0b238f36371cffd6f4
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-06 19:49:56 +0300

    yeni endpointlerin backend yönlendirmeleri

--- backend/routes/membershipRoutes.js
+++ backend/routes/membershipRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { payToMembership, createMembership, getUserMembership } from '../controllers/membershipController.js';
+import { payToMembership, createMembership, getUserMembership, getMemberships, MyMembershipId, deleteMembershipById, updateMembershipMealsToDelivered } from '../controllers/membershipController.js';
 import { admin, protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-router.route('/').get(protect, getUserMembership).post(protect, createMembership);
-// router.route('/:id').delete(protect, getGiftCardByIdAndDelete);
+router.route('/').get(protect, admin, getMemberships).post(protect, createMembership);
+router.route('/myMembershipId').get(protect, MyMembershipId);
 router.route('/pay').post(protect, payToMembership);
+router.route('/:id').get(protect, getUserMembership).delete(protect, admin, deleteMembershipById).put(protect, admin, updateMembershipMealsToDelivered);
+
 export default router;
