87a19b602335812adb21b3c7f881ce587d2ec243
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-08 22:44:25 +0300

    kullanıcı preference seçerse frontta tutuyoruz o preferenceyi.

--- frontend/src/components/membership/ChoosePreferences.jsx
+++ frontend/src/components/membership/ChoosePreferences.jsx
@@ -1,10 +1,13 @@
 import MealPreferences from '../../componentsdata/MealPreferences.js';
 import PreferencesBox from './PreferencesBox.jsx';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const ChoosePreferences = () => {
+  //Kullanıcı bir kere basmışsa localde tutuyoruz ve o plan sayfasında selected değeri için default olarak ayarlıyoruz. Bu değer artık sadece plan satın alınırsa/faillenirse sıfırlanır.(localde)
+  const membershipDetails = useSelector((state) => state.membershipDetail);
   //selected'i localde tutacağız.
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState(membershipDetails.preference);
   return (
     <div className="colOneDiv flex flex-col justify-center items-center  min-[1100px]:mt-10 gap-4  min-[1100px]:pl-12">
       <h2 className="text-[22px] font-semibold text-[#303236]">1. Choose your preferences</h2>
