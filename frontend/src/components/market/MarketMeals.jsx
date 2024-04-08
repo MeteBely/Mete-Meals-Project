31ce230432481ba8e0e858f75a0e995233226b71
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-08 23:16:28 +0300

    market page bug
    yeni oluşturulan meal kit eğer editlenmeden market page girilirse meali olmadığı için(img si) hata geliyordu.

--- frontend/src/components/market/MarketMeals.jsx
+++ frontend/src/components/market/MarketMeals.jsx
@@ -5,7 +5,7 @@ const MarketMeals = ({ mealKit }) => {
     <>
       <div className="fontCera w-[282px] mb-[20px] h-[394px]">
         <Link to={`/market/mealKit/${mealKit._id}`}>
-          <img src={mealKit.meals[0].meal.img} className="cursor-pointer" />
+          <img src={mealKit.meals[0] ? mealKit.meals[0].meal.img : 'https://png.pngtree.com/png-vector/20211025/ourmid/pngtree-stamp-set-red-example-rectangle-png-image_4003403.png'} className="cursor-pointer" />
         </Link>
         <Link to={`/market/mealKit/${mealKit._id}`} className="font-semibold text-[#303236] inline-block mt-4 hover:underline cursor-pointer">
           {mealKit.name}
