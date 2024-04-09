6f6b406de9cc71a14692236f777ab2c0b0fc12bc
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-10 01:35:43 +0300

    bug fix

--- frontend/src/components/market/OrderItem.jsx
+++ frontend/src/components/market/OrderItem.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 const OrderItem = ({ orderItem }) => {
   const { data: firstMealOfKit, isLoading } = useGetMealDetailsQuery(orderItem.meals[0].meal);
+  console.log(orderItem);
   return (
     <>
       {isLoading ? (
@@ -13,7 +14,7 @@ const OrderItem = ({ orderItem }) => {
         <div className="flex flex-row items-center justify-between w-[600px] mb-2">
           <div className="flex flex-row items-center justify-start">
             <img className="w-20 rounded-md mr-4" src={firstMealOfKit.img} alt={orderItem.name} />
-            <Link className="fontCera underline" to={`/market/mealKit/${orderItem._id}`}>
+            <Link className="fontCera underline" to={`/market/mealKit/${orderItem.mealKit}`}>
               {orderItem.name}
             </Link>
           </div>
