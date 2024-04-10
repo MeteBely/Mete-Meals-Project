bf87209045f355dbfa64ac3a9b7fe1c16814ac2a
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-10 17:34:02 +0300

    loader düzeltildi

--- frontend/src/components/common/Loader.jsx
+++ frontend/src/components/common/Loader.jsx
@@ -1,7 +1,7 @@
 import SyncLoader from 'react-spinners/SyncLoader';
 
 const Loader = () => {
-  return <SyncLoader className="w-[200px] mx-auto my-10 text-center" color="#235091" size={20} margin={8} />;
+  return <SyncLoader className="my-10 flex items-center justify-center" color="#235091" size={20} margin={8} />;
 };
 
 export default Loader;
