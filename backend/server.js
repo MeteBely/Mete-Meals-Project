fde8588730deb49865a9bb1f14acd9077cf15452
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-13 18:59:39 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import orderRoutes from './routes/orderRoutes.js';
 import giftCardRoutes from './routes/giftCardRoutes.js';
 import balanceRoutes from './routes/balanceRoutes.js';
 import membershipRoutes from './routes/membershipRoutes.js';
+import path from 'path';
 
 const port = process.env.PORT || 5000;
 
@@ -22,10 +23,6 @@ app.use(express.json()); //req.body --> raw için
 app.use(express.urlencoded({ extended: true })); //req.body --> urlencoded için.
 app.use(cookieParser()); //req.cookie --> cookie için.
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/meals', mealRoutes);
 app.use('/api/mealKits', mealKitsRoutes);
 app.use('/api/users', userRoutes);
@@ -35,6 +32,20 @@ app.use('/api/balance', balanceRoutes);
 app.use('/api/membership', membershipRoutes);
 
 app.get('/api/config/stripe', (req, res) => res.json(process.env.STRIPE_PUBLISHABLE_KEY));
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
