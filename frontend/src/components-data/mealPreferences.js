c3807d23285bb890ba7ffe84deb8bb61c2987822
MeteBely <metehanmuradoglu17@gmail.com>
2024-04-15 00:36:13 +0300

    component data icon yol düzeltmeleri

--- frontend/src/components-data/mealPreferences.js
+++ frontend/src/components-data/mealPreferences.js
@@ -1,37 +1,48 @@
+import chefHatBlack from '../assets/icons/chefHatBlack.png';
+import chefHat from '../assets/icons/chefHat.png';
+import leafBlack from '../assets/icons/leafBlack.png';
+import leaf from '../assets/icons/leaf.png';
+import lessCal from '../assets/icons/lessCal.png';
+import lessCalBlue from '../assets/icons/lessCalBlue.png';
+import lessCarbBlack from '../assets/icons/lessCarbBlack.png';
+import lessCarb from '../assets/icons/lessCarb.png';
+import veggiesBlack from '../assets/icons/veggiesBlack.jpg';
+import veggies from '../assets/icons/veggies.png';
+
 const mealPreferences = [
   {
     title: 'Chef Favorites',
     description: "Our test kitchen's top picks",
-    img: 'src/assets/icons/chefHatBlack.png',
-    imgBlue: 'src/assets/icons/chefHat.png',
+    img: chefHatBlack,
+    imgBlue: chefHat,
     preference: "nutritionist's pick",
   },
   {
     title: 'High Protein',
     description: 'Eat for your muscles bruh',
-    img: 'src/assets/icons/leafBlack.png',
-    imgBlue: 'src/assets/icons/leaf.png',
+    img: leafBlack,
+    imgBlue: leaf,
     preference: '45g of protein',
   },
   {
     title: 'Less Calories',
     description: 'Eat our less calories meals and stay always fit',
-    img: 'src/assets/icons/lessCal.png',
-    imgBlue: 'src/assets/icons/lessCalBlue.png',
+    img: lessCal,
+    imgBlue: lessCalBlue,
     preference: '600 calories or less',
   },
   {
     title: 'Less Carb',
     description: 'If you are in the diet, this is the best option for your body',
-    img: 'src/assets/icons/lessCarbBlack.png',
-    imgBlue: 'src/assets/icons/lessCarb.png',
+    img: lessCarbBlack,
+    imgBlue: lessCarb,
     preference: 'carb conscious',
   },
   {
     title: 'Veggies',
     description: 'Meat-free meals highlighting seasonally inspired produce',
-    img: 'src/assets/icons/veggiesBlack.jpg',
-    imgBlue: 'src/assets/icons/veggies.png',
+    img: veggiesBlack,
+    imgBlue: veggies,
     preference: 'vegetarian',
   },
 ];
